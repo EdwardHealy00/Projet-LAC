701158a200d8d627cf36fc2fa2a6f421099aa6f6
Yanis Toubal <yanis.toubal@polymtl.ca>
2022-07-04 22:46:23 -0400

    progress case study with connection

--- etude-cas/src/components/App.tsx
+++ etude-cas/src/components/App.tsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
  import Catalogue from './Catalogue';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CaseStudyWTconnection from './CaseStudyWTconnection';
+import CaseStudyWconnection from './CaseStudyWconnection';
 
 function App() {
   // const [count, setCount] = useState(0);
   return (
     <div>
         <Routes>
+          <Route path="/" element={<Catalogue />} />
           <Route path="/catalogue" element={<Catalogue />} />
-          <Route path="/etude-de-cas" element={<CaseStudyWTconnection />} />
+          {/* <Route path="/etude-de-cas" element={<CaseStudyWTconnection />} /> */}
+          <Route path="/etude-de-cas" element={<CaseStudyWconnection />} />
           {/* <Route path="/contact" element={< />} /> */}
         </Routes>
     </div>
---
+++ etude-cas/src/components/CaseStudyWconnection.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import '../styles/LightCaseStudy.css';
+import Button from './UI-Elements/Button';
+import '../img/normal_search.svg';
+// import { useNavigate } from "react-router-dom";
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+interface Props {}
+
+const CaseStudyWTconnection: React.FC<Props> = ({ }) => { 
+    // const navigate = useNavigate();
+  return (
+    <div id="main">
+      <div id="profile">
+        <AccountCircle sx={{verticalAlign: 'middle', fontSize:'32px'}}/>Professeur
+      </div>
+        <div id="line"></div>
+        <Button children="> Retour au catalogue" styleName="return" onClick={() => console.log("You clicked")}></Button>
+        <div id="part">
+          <div id="top-part">
+            <div id="first">
+              <div id="title">
+                Littéralement en changement
+                <div id="subtitle">
+                  L’impact de la COVID-19 sur l’écosystème littéraire québécois
+                </div>
+              </div>
+
+              <div id="information">Au début de l’année 2020, la pandémie de la COVID-19 a frappé de plein fouet le milieu littéraire. Fragilisé, l’écosystème littéraire a subi une énième vague de transformation. Les bibliothèques ont fermé. Les salons du livre, les lancements et les événements culturels ont été reportés ou carrément annulés. Les retards de paiement des éditeurs s’accumulent. Le droit de représentation empêche bon nombre d’auteurs et d’autrices de présenter leurs livres, même virtuellement. L’industrie du livre est-elle en crise?</div>
+            </div>
+            <div id="second">
+              <div><b>Auteurs :</b> Virginie Francoeur, Annie Passalacqua</div>
+              <div><b>Discipline :</b> Génie industriel</div>
+              <div><b>Sujet(s) :</b> Gestion du changement</div>
+              <div><b>Nombre de pages :</b> 9</div>
+              <div><b>Date :</b> 2021-02-28</div>
+
+            </div>
+          </div>
+          <div id="bottom-part">
+
+          </div>
+        </div>
+
+    </div>
+  );
+}
+
+export default CaseStudyWTconnection;
