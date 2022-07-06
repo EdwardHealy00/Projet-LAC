3654cee7386d934c547089905cddbb82e9ee4a48
Yanis Toubal <yanis.toubal@polymtl.ca>
2022-07-06 12:39:34 -0400

    using same layout for case study pages

--- etude-cas/src/components/CaseStudyWTconnection.tsx
+++ etude-cas/src/components/CaseStudyWTconnection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../styles/LightCaseStudy.scss';
-import Button from './UI-Elements/Button';
+import Button from '@mui/material/Button';
 import '../img/normal_search.svg';
 // import { useNavigate } from "react-router-dom";
 
@@ -10,32 +10,36 @@ const CaseStudyWTconnection: React.FC<Props> = ({ }) => {
     // const navigate = useNavigate();
   return (
     <div id="main">
-        <Button children="Se Connecter" styleName="se-connecter" onClick={() => console.log("You clicked")}></Button>
-        <div id="line"></div>
-        <Button children="> Retour au catalogue" styleName="return" onClick={() => console.log("You clicked")}></Button>
-        <div id="part">
-          <div id="top-part">
+      <div id="profile">
+        <Button variant="contained" onClick={() => console.log("You clicked")}>Se Connecter</Button>
+      </div>
+        <div id="main-line"></div>
+        <div>
+        <Button className="return" onClick={() => console.log("You clicked")}>&gt; Retour au catalogue</Button>
+          <div className="section">
+            <div className="section-line"></div>
             <div id="first">
               <div id="title">
                 Littéralement en changement
+                <div id="subtitle">
+                  L’impact de la COVID-19 sur l’écosystème littéraire québécois
+                </div>
               </div>
-              {/* <div id="title"> TODO
-                L’impact de la COVID-19 sur l’écosystème littéraire québécois
-              </div> */}
-              <div id="information">Au début de l’année 2020, la pandémie de la COVID-19 a frappé de plein fouet le milieu littéraire. Fragilisé, l’écosystème littéraire a subi une énième vague de transformation. Les bibliothèques ont fermé. Les salons du livre, les lancements et les événements culturels ont été reportés ou carrément annulés. Les retards de paiement des éditeurs s’accumulent. Le droit de représentation empêche bon nombre d’auteurs et d’autrices de présenter leurs livres, même virtuellement. L’industrie du livre est-elle en crise?</div>
-            </div>
-            <div id="second">
-              <div>Auteurs : Virginie Francoeur, Annie Passalacqua</div>
-              <div>Discipline : Génie industriel</div>
-              <div>Sujet(s) : Gestion du changement</div>
-              <div>Nombre de pages : 9</div>
-              <div>Date :   2021-02-28</div>
+              <br />
+              <div id="information">
+                <div>Au début de l’année 2020, la pandémie de la COVID-19 a frappé de plein fouet le milieu littéraire. Fragilisé, l’écosystème littéraire a subi une énième vague de transformation. Les bibliothèques ont fermé. Les salons du livre, les lancements et les événements culturels ont été reportés ou carrément annulés. Les retards de paiement des éditeurs s’accumulent. Le droit de représentation empêche bon nombre d’auteurs et d’autrices de présenter leurs livres, même virtuellement. L’industrie du livre est-elle en crise?</div>
+                <div>
+                  <div><b>Auteurs :</b> Virginie Francoeur, Annie Passalacqua</div>
+                  <div><b>Discipline :</b> Génie industriel</div>
+                  <div><b>Sujet(s) :</b> Gestion du changement</div>
+                  <div><b>Nombre de pages :</b> 9</div>
+                  <div><b>Date :</b> 2021-02-28</div>
+                </div>
+              </div>
 
             </div>
-          </div>
-          <div id="bottom-part">
-
-          </div>
+              
+            </div>
         </div>
 
     </div>
--- etude-cas/src/components/CaseStudyWconnection.tsx
+++ etude-cas/src/components/CaseStudyWconnection.tsx
@@ -14,10 +14,11 @@ const CaseStudyWTconnection: React.FC<Props> = ({ }) => {
       <div id="profile">
         <AccountCircle sx={{verticalAlign: 'middle', fontSize:'32px'}}/>Professeur
       </div>
-        <div id="line"></div>
+        <div id="main-line"></div>
+        <div>
         <Button className="return" onClick={() => console.log("You clicked")}>&gt; Retour au catalogue</Button>
-        <div id="part">
-          <div id="top-part">
+          <div className="section">
+            <div className="section-line"></div>
             <div id="first">
               <div id="title">
                 Littéralement en changement
@@ -25,22 +26,28 @@ const CaseStudyWTconnection: React.FC<Props> = ({ }) => {
                   L’impact de la COVID-19 sur l’écosystème littéraire québécois
                 </div>
               </div>
+              <br />
+              <div id="information">
+                <div>Au début de l’année 2020, la pandémie de la COVID-19 a frappé de plein fouet le milieu littéraire. Fragilisé, l’écosystème littéraire a subi une énième vague de transformation. Les bibliothèques ont fermé. Les salons du livre, les lancements et les événements culturels ont été reportés ou carrément annulés. Les retards de paiement des éditeurs s’accumulent. Le droit de représentation empêche bon nombre d’auteurs et d’autrices de présenter leurs livres, même virtuellement. L’industrie du livre est-elle en crise?</div>
+                <div>
+                  <div><b>Auteurs :</b> Virginie Francoeur, Annie Passalacqua</div>
+                  <div><b>Discipline :</b> Génie industriel</div>
+                  <div><b>Sujet(s) :</b> Gestion du changement</div>
+                  <div><b>Nombre de pages :</b> 9</div>
+                  <div><b>Date :</b> 2021-02-28</div>
+                </div>
+              </div>
 
-              <div id="information">Au début de l’année 2020, la pandémie de la COVID-19 a frappé de plein fouet le milieu littéraire. Fragilisé, l’écosystème littéraire a subi une énième vague de transformation. Les bibliothèques ont fermé. Les salons du livre, les lancements et les événements culturels ont été reportés ou carrément annulés. Les retards de paiement des éditeurs s’accumulent. Le droit de représentation empêche bon nombre d’auteurs et d’autrices de présenter leurs livres, même virtuellement. L’industrie du livre est-elle en crise?</div>
+            </div>
+              
               <Button id="use-case" variant="contained" onClick={() => console.log("You clicked")}>Utiliser ce cas</Button>
               <Button variant="outlined" onClick={() => console.log("You clicked")}>Consulter</Button>
             </div>
-            <div id="second">
-              <div><b>Auteurs :</b> Virginie Francoeur, Annie Passalacqua</div>
-              <div><b>Discipline :</b> Génie industriel</div>
-              <div><b>Sujet(s) :</b> Gestion du changement</div>
-              <div><b>Nombre de pages :</b> 9</div>
-              <div><b>Date :</b> 2021-02-28</div>
-
+          <div className="section">
+            <div className="section-line"></div>
+            <div id="usage-historic">
+              <h3>Historique d'utilisation</h3>
             </div>
-          </div>
-          <div id="usage-historic">
-            <h3>Historique d'utilisation</h3>
           </div>
         </div>
 
