ca3f8bbccb5f4613d610d50276fc1a53fab059dd
Yanis Toubal <yanis.toubal@polymtl.ca>
2022-07-23 19:09:31 -0400

    fixed menu

--- client/src/components/common/NavBar.tsx
+++ client/src/components/common/NavBar.tsx
@@ -1,24 +1,27 @@
-import { AccountCircle } from "@mui/icons-material";
 import { Button, ButtonGroup } from "@mui/material";
 import React from "react";
-import '../../styles/NavBar.scss';
+import './NavBar.scss';
 import Login from "../connection/Login";
-import Register from "../connection/Register";
 
 interface Props { }
 
 const NavBar: React.FC<Props> = ({ }) => {
+  
     return (
-      <div id="profile">
-        {/* <ButtonGroup
-          variant="contained"
-          aria-label="outlined primary button group"
-        >
-          <Button>Catalogue</Button>
-          <Button>Dashboard</Button>
-          <Button>À propos</Button>
-        </ButtonGroup> */}
-        <Login />
+      <div id="navbar">
+        <div id="profile">
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button href="/catalogue">Catalogue</Button>
+            <Button href="/dashboard">Tableau de board</Button>
+            <Button>À propos</Button>
+          </ButtonGroup>
+        </div>
+        <div id="loginStatus">
+          <Login />
+        </div>
       </div>
     );
 }
