2004455d89a9331b1256afd0d2980bcbafbbb170
Yanis Toubal <yanis.toubal@polymtl.ca>
2022-07-25 22:04:39 -0400

    css tweaks

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
- import Catalogue from './catalogue/Catalogue';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import Catalogue from './catalogue/Catalogue';
+import { Routes, Route } from "react-router-dom";
 import CaseStudyWconnection from './caseStudy/CaseStudyWconnection';
 import NavBar from './common/NavBar';
 import CollaborativeSpace from './collaborativeSpace/collaborativeSpace';
@@ -15,24 +15,26 @@ function App() {
   return (
     <div id="main">
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        {/* <Route path="/etude-de-cas" element={<CaseStudyWTconnection />} /> */}
-        <Route path="/etude-de-cas" element={<CaseStudyWconnection />} />
-        {/* <Route path="/contact" element={< />} /> */}
-        <Route
-          path="/espace-de-collaboration"
-          element={<CollaborativeSpace />}
-        />
-        <Route path="/dashboard" element={<DashboardPaidCase />} />
-        <Route path="/approval" element={<Approval />} />
-        <Route path="/new-case-approval" element={<NewCase />} />
-        {/* <Route
-          path="/espace-de-collaboration/etude-de-cas"
-          element={<CaseStudy />}
-        /> */}
-      </Routes>
+      <div id="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          {/* <Route path="/etude-de-cas" element={<CaseStudyWTconnection />} /> */}
+          <Route path="/etude-de-cas" element={<CaseStudyWconnection />} />
+          {/* <Route path="/contact" element={< />} /> */}
+          <Route
+            path="/espace-de-collaboration"
+            element={<CollaborativeSpace />}
+          />
+          <Route path="/dashboard" element={<DashboardPaidCase />} />
+          <Route path="/approval" element={<Approval />} />
+          <Route path="/new-case-approval" element={<NewCase />} />
+          {/* <Route
+            path="/espace-de-collaboration/etude-de-cas"
+            element={<CaseStudy />}
+          /> */}
+        </Routes>
+      </div>
     </div>
   );
 }
