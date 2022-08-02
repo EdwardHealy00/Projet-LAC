bcab8c1871ed7a8f0f3bd54643ae50bee1133082
Yanis Toubal <yanis.toubal@polymtl.ca>
2022-08-02 18:49:38 -0400

    added comity and polypress approval

---
+++ client/src/components/polyPress/Approval.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Case } from "../../model/Case";
+import "../../styles/Approval.scss";
+import CaseTable from "./CaseTable";
+
+function createData(
+  id_: number,
+  title: string,
+  author: string,
+  submitted: string,
+  status: string
+): Case {
+  return { id_, title, author, submittedDate: submitted, status };
+}
+
+const rowsStep1: Case[] = [
+  createData(
+    53,
+    "Mesurer la qualité des produits dans les simulations ABC",
+    "Jane Doe",
+    "2022/05/23",
+    "Nouveau"
+  ),
+];
+
+function Approval() {
+  return (
+    <div id="approvalPage">
+      <div className="stepApproval">
+        <b>
+          <h2>Actions à prendre</h2>
+        </b>
+        <CaseTable cases={rowsStep1} />
+      </div>
+    </div>
+  );
+}
+
+export default Approval;
---
+++ client/src/components/polyPress/CaseTable.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { Case } from "../../model/Case";
+
+interface CaseProp {
+    cases: Case[];
+}
+
+export default function CaseTable(rows: CaseProp) {
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>#</TableCell>
+            <TableCell align="right">Titre du cas</TableCell>
+            <TableCell align="right">Auteur</TableCell>
+            <TableCell align="right">Soumis le</TableCell>
+            <TableCell align="right">Statut</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.cases.map((row) => (
+            <TableRow
+              key={row.id_}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id_}
+              </TableCell>
+              <TableCell align="right">{row.title}</TableCell>
+              <TableCell align="right">{row.author}</TableCell>
+              <TableCell align="right">{row.submittedDate}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">
+                <Button
+                  variant="contained"
+                  sx={{ backgroundColor: "#ff9b00" }}
+                >
+                  Traiter
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
