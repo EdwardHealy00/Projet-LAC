7724928bba2d88a09e70d348c911b6bc2136f52b
Yanis Toubal <yanis.toubal@polymtl.ca>
2022-08-15 16:10:19 -0400

    password validation done

--- client/src/components/connection/ResetPassword.tsx
+++ client/src/components/connection/ResetPassword.tsx
@@ -2,21 +2,43 @@ import { useParams } from "react-router-dom";
 import React from "react";
 import axios from "axios";
 import { UserPasswordReset } from "../../model/User";
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+} from "@mui/material";
+import { VisibilityOff, Visibility } from "@mui/icons-material";
 
 export default function ResetPassword() {
   const { resetToken } = useParams();
 
-  const initialStateErrors = {
-    password: "",
-    confirmPassword: "",
-  };
+  const [passwordError, setPasswordError] = React.useState({
+      isError: false,
+      message: "",
+  });
 
-  const [error, setError] = React.useState(initialStateErrors);
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const validatePassword = (password: string) => {
+    const passwordError = {
+      isError: false,
+      message: "",
+    };
+    if (password.length < 8 || password.length > 32) {
+      passwordError.isError = true;
+      passwordError.message = "Le mot de passe doit contenir entre 8 et 32 caractères";
+    }
+    setPasswordError(passwordError);
+    return passwordError.isError;
+  };
 
   const handleSubmitNewPassword = (e: any) => {
     e.preventDefault();
-    console.log(e.target.elements);
+    const password = e.target.password.value.trim();
+    if (!validatePassword(password)) return;
     const newPassword: UserPasswordReset = {
       reset_token: resetToken!,
       password: e.target.password.value,
@@ -41,28 +63,30 @@ export default function ResetPassword() {
     <div>
       <div>Choissisez un nouveau mot de passe</div>
       <form onSubmit={handleSubmitNewPassword}>
-        <div>
-          <TextField
-            required
-            autoFocus
-            margin="dense"
-            label="Mot de passe"
-            type="password"
-            variant="outlined"
+        <FormControl variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">
+            Nouveau mot de passe
+          </InputLabel>
+          <Input
+            error={passwordError.isError}
             name="password"
+            id="standard-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
           />
-        </div>
-        <div>
-          <TextField
-            required
-            autoFocus
-            margin="dense"
-            label="Confirmer le mot de passe"
-            type="password"
-            variant="outlined"
-            name="passwordConfirm"
-          />
-        </div>
+        </FormControl>
+        {passwordError.isError && (
+          <div style={{ color: "red" }}>{passwordError.message}</div>
+        )}
         <Button type="submit" variant="contained">
           Valider
         </Button>
