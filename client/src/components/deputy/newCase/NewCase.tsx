d1ca597e86bf911b3df56abd97f0035150e54f57
chpoud <charles.poulin@live.ca>
2023-06-19 10:27:24 -0400

    Updated initial criterials for approval

--- client/src/components/deputy/newCase/NewCase.tsx
+++ client/src/components/deputy/newCase/NewCase.tsx
@@ -21,12 +21,10 @@ export const checkList: string[] = [
   "L’étude de cas est en format Word.",
   "L’étude de cas respecte le gabarit fourni.",
   "Les notes pédagogiques sont incluses dans l’étude de cas.",
-  "La version étudiante est incluse dans l’étude de cas.",
   "Toutes les sections dans le gabarit sont dûment remplies.",
   "La matière semble pertinente au génie, aux sciences ou à la technologie.",
   "La qualité rédactionnelle est satisfaisante.",
-  "Le formulaire d’autorisation de publication est en format PDF.",
-  "Le formulaire d’autorisation est dûment rempli (signatures, dates, etc.)",
+  "Le formulaire de consentement est dûment rempli (signatures, dates, etc.)",
   "Tous les fichiers pertinents à l’étude de cas sont présents (données, annexes, outils, etc.)",
 ];
 
