6a1e80d16025f5cbf5b5e5e6bfdbb166f8d4b271
Edward Healy <edwardhealy87@gmail.com>
2023-08-29 19:22:54 -0400

    GLobal theme and styling, revamped catalogue articles

--- client/src/utils/FileDownloadUtil.tsx
+++ client/src/utils/FileDownloadUtil.tsx
@@ -2,24 +2,28 @@ import axios from "axios";
 
 export function handleDownloadAll(files: any[])  {
     for (let i = 0; i < files.length; i++) {
-      axios
-        .get(
-          `${process.env.REACT_APP_BASE_API_URL}/api/caseStudies/download/` +
-            files[i].filename,
-          {
-            withCredentials: true,
-            responseType: "arraybuffer",
-          }
-        )
-        .then((res) => {
-          const url = window.URL.createObjectURL(new Blob([res.data]));
-          const link = document.createElement("a");
-          link.href = url;
-          link.setAttribute("download", files[i].originalname); //or any other extension
-          document.body.appendChild(link);
-          link.click();
-        });
+      handleFileDownload(files[i]);
     }
+};
+
+export function handleFileDownload(file: any) {
+  axios
+    .get(
+      `${process.env.REACT_APP_BASE_API_URL}/api/caseStudies/download/` +
+        file.filename,
+      {
+        withCredentials: true,
+        responseType: "arraybuffer",
+      }
+    )
+    .then((res) => {
+      const url = window.URL.createObjectURL(new Blob([res.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", file.originalname); //or any other extension
+      document.body.appendChild(link);
+      link.click();
+    });
 };
 
 export function downloadCaseStudyTemplate() {
