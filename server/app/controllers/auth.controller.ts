2c16128488f02188857dee02d3ee50f633145f69
Edward Healy <edwardhealy87@gmail.com>
2023-09-26 21:11:25 -0400

    Changed cookie options to accept cross-site

--- server/app/controllers/auth.controller.ts
+++ server/app/controllers/auth.controller.ts
@@ -99,8 +99,8 @@ export class AuthController {
                         Date.now() + parseInt(ACCESS_TOKEN_EXPIRES_IN) * 60 * 1000
                     ),
                     maxAge: parseInt(ACCESS_TOKEN_EXPIRES_IN) * 60 * 1000,
-       //             httpOnly: true,
-                    sameSite: 'strict',
+                    secure: true,
+                    sameSite: 'none',
                 };
 
                 // Send Access Token in Cookie
