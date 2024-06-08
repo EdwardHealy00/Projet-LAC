1b8c481b0e108b9e992c461865821a052531c203
Edward Healy <edwardhealy87@gmail.com>
2024-06-08 15:37:09 -0400

    Missions and spécificités sections now responsive

--- client/src/components/landingPage/AboutPage.tsx
+++ client/src/components/landingPage/AboutPage.tsx
@@ -162,10 +162,10 @@ export default function AboutPage() {
         width: 56, 
         height: 56,
         [theme.breakpoints.down('sm')]: {
-            width: 48, 
-            height: 48, 
             bottom: 12,
             right: 12,
+            width: 48, 
+            height: 48, 
         },
     };
     
@@ -191,7 +191,7 @@ export default function AboutPage() {
             </header>
             <div className="landing-segment-container">
                 <div className="landing-segment-column-left">
-                    <h1 id="a-propos">À propos</h1>
+                    <h1 id="a-propos" className="subtitle">À propos</h1>
                     <div className="value-paragraphs-container">
                         <LandingValueGroupParagraph content="des connaissances grâce à une plateforme collaborative" title={"Partager".toUpperCase()}></LandingValueGroupParagraph>
                         <LandingValueGroupParagraph content="à un catalogue d’études de cas pour faciliter l’enseignement par les cas" title={"Accéder".toUpperCase()}></LandingValueGroupParagraph>
@@ -203,19 +203,19 @@ export default function AboutPage() {
             <BrainSeparator></BrainSeparator>
             <div className="landing-segment-container">
                 <div className="landing-segment-column-left">
-                    <h1 id="mission">Mission, vision, pertinence et compétences promues</h1>
+                    <h1 className="subtitle" id="mission">Mission, vision, pertinence et compétences promues</h1>
                     <div className="mission-container">
                         <div className="colored-tag-pair-container">
                             <ColoredTag tag="MISSION" backgroundColor="#FA961E"></ColoredTag>
-                            <div>Proposer des approches pédagogiques diversifiées et adaptées afin de favoriser l’acquisition de nouvelles compétences par les ingénieures et ingénieurs et les gestionnaires de demain en rendant la formation plus professionnalisante et plus axée sur les aspects humains et environnementaux.</div>
+                            <div className="p-reactive">Proposer des approches pédagogiques diversifiées et adaptées afin de favoriser l’acquisition de nouvelles compétences par les ingénieures et ingénieurs et les gestionnaires de demain en rendant la formation plus professionnalisante et plus axée sur les aspects humains et environnementaux.</div>
                         </div>
                         <div className="colored-tag-pair-container">
                             <ColoredTag tag="VISION" backgroundColor="#7FC348"></ColoredTag>
-                            <div>Devenir une référence en innovation pédagogique en génie et sciences sociales au sein de Polytechnique Montréal et dans la francophonie.</div>
+                            <div className="p-reactive">Devenir une référence en innovation pédagogique en génie et sciences sociales au sein de Polytechnique Montréal et dans la francophonie.</div>
                         </div>
                         <div className="colored-tag-pair-container">
                             <ColoredTag tag="PERTINENCE" backgroundColor="#C3272E"></ColoredTag>
-                            <div>Les tendances technologiques, principalement la robotisation, la numérisation et l’intelligence artificielle entraîneront une transformation des milieux de travail et des relations entre les personnes. Ces changements nécessiteront une adaptation, une agilité et une flexibilité des étudiantes et étudiants, et c’est notamment par l’entremise des études de cas que ces compétences pourront être acquises plus rapidement.</div>
+                            <div className="p-reactive">Les tendances technologiques, principalement la robotisation, la numérisation et l’intelligence artificielle entraîneront une transformation des milieux de travail et des relations entre les personnes. Ces changements nécessiteront une adaptation, une agilité et une flexibilité des étudiantes et étudiants, et c’est notamment par l’entremise des études de cas que ces compétences pourront être acquises plus rapidement.</div>
                         </div>
                         <div className="colored-tag-pair-container">
                             <ColoredTag tag="COMPÉTENCES" backgroundColor="#FA961E"></ColoredTag>
@@ -237,15 +237,15 @@ export default function AboutPage() {
                             </div>
                         </div>
                     </div>
-                    <h1 id="besoin">Besoins auxquels répond le LAC</h1>
+                    <h1 id="besoin" className="subtitle">Besoins auxquels répond le LAC</h1>
                     <div className="needs-container">
-                        <p className="paragraph-font">
+                        <p className="paragraph-font p-reactive">
                             À la lumière des nombreuses tendances qui influent sur la pratique de l’ingénierie, un <a href="https://www.oiq.qc.ca/wp-content/uploads/documents/public/Etude_OIQ_Profil_Ing_2021.pdf" target="_blank">récent rapport de l’Ordre des ingénieurs du Québec (2021)</a> soutient que les diplômé·e·s devront non seulement maîtriser des compétences techniques inhérentes à leur discipline, mais devront également déployer de nombreuses autres compétences relevant davantage du champ des sciences sociales et humaines. Le rôle des ingénieures et ingénieurs est résolument transversal, il ne se limite plus à la compréhension et à la maîtrise des enjeux techniques, mais englobent également des enjeux sociaux.
                         </p>
-                        <p className="paragraph-font">
+                        <p className="paragraph-font p-reactive">
                             Par ailleurs, les compétences sociales ne semblent pas être totalement maîtrisées par les futurs ingénieures et ingénieurs. En effet, lors d’un sondage mené par l’Ordre des ingénieurs (2021), un constat est frappant : « malgré une solide formation universitaire sur le plan des compétences dites dures, plusieurs ont mentionné ne pas être suffisamment outillés à la sortie de l’université pour gérer cette complexité d’un point de vue humain » (p.91). C’est pour répondre à ces défis que le LAC a été créé.
                         </p>
-                        <p className="paragraph-font">
+                        <p className="paragraph-font p-reactive">
                             L’apprentissage par les cas offre des avantages uniques : il met l’accent sur le développement des compétences sociales et humaines, favorise les échanges d’idées et permet d’analyser de réelles problématiques organisationnelles et industrielles dans le but d’accélérer l’entrée sur le marché du travail des étudiantes et étudiants et leur prise de décision dans les organisations.
                         </p>
                     </div>
@@ -254,7 +254,7 @@ export default function AboutPage() {
             <BrainSeparator></BrainSeparator>
             <div className="landing-segment-container">
                 <div className="landing-segment-column-left">
-                    <h1 id="mission">Spécificités du LAC</h1>
+                    <h1 id="mission" className="subtitle">Spécificités du LAC</h1>
                     <div className="all-items-margins">
                         <div className="item-lists-container">
                             <CircleIcon color="#C3272E" href={TEACH}></CircleIcon>
